be5798eee1e1bfb0d1677db22db82a0a666f98c9
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-09-09 20:49:01 +0700

    update: add repository annotations

--- pelanggan_service/src/main/java/com/furqon/pelanggan_service/repository/PelangganRepository.java
+++ pelanggan_service/src/main/java/com/furqon/pelanggan_service/repository/PelangganRepository.java
@@ -1,9 +1,11 @@
 package com.furqon.pelanggan_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.furqon.pelanggan_service.model.Pelanggan;
 
+@Repository
 public interface PelangganRepository extends JpaRepository<Pelanggan, Long> {
 
 }
