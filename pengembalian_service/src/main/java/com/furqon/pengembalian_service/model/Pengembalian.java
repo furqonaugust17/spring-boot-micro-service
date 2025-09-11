2e3502784256de67ab16484b79bda85546ee2549
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-09-11 21:01:50 +0700

    feat: create pengembalian service

---
+++ pengembalian_service/src/main/java/com/furqon/pengembalian_service/model/Pengembalian.java
@@ -0,0 +1,21 @@
+package com.furqon.pengembalian_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "pengembalian")
+public class Pengembalian {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long peminjamanId;
+    private String tanggalDikembalikan;
+    private String terlambat;
+    private double denda;
+}
