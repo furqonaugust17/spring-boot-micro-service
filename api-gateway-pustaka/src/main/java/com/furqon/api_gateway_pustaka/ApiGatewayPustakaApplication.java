ae3facadcfd8233d7a277f4b053274812bb5b8ca
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-09-17 10:20:53 +0700

    feat: create gateway for pustaka

---
+++ api-gateway-pustaka/src/main/java/com/furqon/api_gateway_pustaka/ApiGatewayPustakaApplication.java
@@ -0,0 +1,13 @@
+package com.furqon.api_gateway_pustaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayPustakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayPustakaApplication.class, args);
+	}
+
+}
---
+++ api-gateway-pustaka/src/test/java/com/furqon/api_gateway_pustaka/ApiGatewayPustakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furqon.api_gateway_pustaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayPustakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
