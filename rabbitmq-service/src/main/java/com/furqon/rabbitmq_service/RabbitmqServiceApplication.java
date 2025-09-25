dea9ebe680dae33e84da79f794f571f54bd2e56b
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-09-25 07:13:09 +0700

    feat: create rabbitmq for order

---
+++ rabbitmq-service/src/main/java/com/furqon/rabbitmq_service/RabbitmqServiceApplication.java
@@ -0,0 +1,47 @@
+package com.furqon.rabbitmq_service;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@SpringBootApplication
+public class RabbitmqServiceApplication {
+	@Value("${app.mail.host}")
+    private String host;
+	
+	@Value("${app.mail.port}")
+    private Integer port;
+
+	@Value("${app.mail.username}")
+    private String username;
+	
+	@Value("${app.mail.password}")
+    private String password;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqServiceApplication.class, args);
+	}
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost(host);
+		mailSender.setPort(port);
+
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+	}
+}
